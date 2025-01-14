07733cea05926d1d4d8447c8ffac8b20119aab82
dheeraj-hj <ee1221704@iitd.ac.in>
2025-01-14 11:01:15 +0530

    Initial header files

---
+++ src/command_handler.h
@@ -0,0 +1,10 @@
+#ifndef COMMAND_HANDLER_H
+#define COMMAND_HANDLER_H
+
+void parse_command(const char *command); // Parse the command 
+void execute_command(); // Execute the command
+int validate_command(); // Validate the command
+
+// Still more functions to be added
+
+#endif
---
+++ src/spreadsheet.h
@@ -0,0 +1,23 @@
+#ifndef SPREADSHEET_H
+#define SPREADSHEET_H
+
+// Single Cell Structure in the Spreadsheet
+typedef struct{
+    char *val; // Value or dependent formula of the cell
+    int dependency; // 0 or 1, 1 if the cell has a dependent formula
+}cell;
+
+
+// Spreadsheet Structure
+typedef struct{
+    cell **sheet; // 2D array of cells
+    int rows; // Number of rows in the spreadsheet
+    int cols; // Number of columns in the spreadsheet
+}spreadsheet;
+
+spreadsheet *create_spreadsheet(int rows, int cols); // Create a new spreadsheet
+void free_spreadsheet(spreadsheet *sheet); // Free the memory allocated for the spreadsheet
+void set_cell(spreadsheet *sheet, int row, int col, char *val); // Set the value of a cell
+int evaluate_cell(spreadsheet *sheet, int row, int col); // Evaluate the value of a cell
+
+#endif
