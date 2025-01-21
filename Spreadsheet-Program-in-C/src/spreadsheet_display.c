513b0656af34b089a80b1ccf5e6538840fb0f7a2
evans-prince <princeevans2004@gmail.com>
2025-01-22 05:16:21 +0530

    Minor changes in display function for alignment.

--- Spreadsheet-Program-in-C/src/spreadsheet_display.c
+++ Spreadsheet-Program-in-C/src/spreadsheet_display.c
@@ -11,17 +11,18 @@ void display_spreadsheet(spreadsheet *sheet){ // Display the spreadsheet
 // this function should print :
 // first line : tab  first_col_name (A , B ,C ) tab  second_col_name and last_col
 // second line : first row_name (1,2,3)  tab value of cell(r1,c1) and so on
-
+    int col_width=10;
+    
         printf("\t");
         for (int i =*(sheet->bounds->first_col)-1; i<=*(sheet->bounds->last_col)-1 ; i++){
                 char* col_label=colIndex_to_name(i);//index i is passed starts from 0
-                printf("%s\t", col_label);
+                printf("%-*s\t",col_width, col_label);
         }
         printf("\n");
         for(int i= *(sheet->bounds->first_row)-1;i<=*(sheet->bounds->last_row)-1;i++){
             printf("%d\t",i+1);
             for (int j =*(sheet->bounds->first_col)-1; j<=*(sheet->bounds->last_col) -1; j++){
-                 printf("%s\t", sheet->table[i][j].val);
+                printf("%-*s\t",col_width, sheet->table[i][j].val);
             }
             printf("\n");
         }
